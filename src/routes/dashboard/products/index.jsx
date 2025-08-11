1fbdd2bbdaa9e163df7cbd2aa5c2086dcf72b090
AlbertUmoren20 <albertumoren2004@gmail.com>
2025-08-11 12:58:27 +0100

    Resolved issue for production

--- src/routes/dashboard/products/index.jsx
+++ src/routes/dashboard/products/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useStandards } from './contexts/standardscontext';
+import { useStandards } from '@/contexts/standardscontext.jsx';
 import { Button, Box, TextField, Typography, Paper, CircularProgress } from '@mui/material';
 // import slugify from 'slugify';
 
